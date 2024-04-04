37f3a09a2c798123b5a86790fa11a225161f2461
Ramesh <varanasiramesh@gmail.com>
2024-04-04 07:34:09 +0000

    some chnages

--- libs/products/src/lib/product-list/product-list.tsx
+++ libs/products/src/lib/product-list/product-list.tsx
@@ -7,6 +7,7 @@ export function ProductList(props: ProductListProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to ProductList!</h1>
+      <p>This is a change. 👋</p>
     </div>
   );
 }
